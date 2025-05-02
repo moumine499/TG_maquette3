97b3e7ad53810d466b6f8dbfa8136af4c25d64f1
ABDEL-ABBO <abdelkerimalisalehabbo@gmail.com>
2025-05-02 16:21:07 +0100

    ajoute de Js et alert

--- slide.js
+++ slide.js
@@ -1,17 +1,30 @@
-let slideIndex = 1;
-showSlides(slideIndex);function plusSlide(n) {
-  showSlides(slideIndex += n);
-}function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}function prevSlide() {
-  plusSlide(-1);
-}function nextSlide() {
-  plusSlide(1);
+let slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+        const slides = document.getElementsByClassName("slide");
+        for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+          slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000); // Change d'image toutes les 3 secondes
+      }
+function ajouterPanier(produit){
+  alert(produit + " à ete ajouter")
+
+  quantite = document.getElementById("quantite");
+quantite = parseInt(quantite.innertHtml);
+quantite.innertHtml = quantite + 1;
+
+  
 }
+
+
+      
+    
