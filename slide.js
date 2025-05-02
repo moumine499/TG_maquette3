8a45ff1fd765a082ba923b5aca9519e979218039
ABDEL-ABBO <abdelkerimalisalehabbo@gmail.com>
2025-05-03 00:20:37 +0100

    lors du survole du curseur qui va s'afficher les info sur le produit la quantite et le nom du prosuit

--- slide.js
+++ slide.js
@@ -16,15 +16,22 @@ let slideIndex = 0;
         setTimeout(showSlides, 3000); // Change d'image toutes les 3 secondes
       }
 function ajouterPanier(produit){
-  alert(produit + " à ete ajouter")
+  alert(produit + " a été ajouté au panier")
 
+
+  // Mettre à jour la quantité du produit dans le panier
   quantite = document.getElementById("quantite");
-quantite = parseInt(quantite.innertHtml);
-quantite.innertHtml = quantite + 1;
+  qte = parseInt(quantite.innerHTML);
+  quantite.innerHTML = qte + 1;   
+  // Mettre en commentaire la partie alert lors du survole du curseur qui affiche les informations sur le produit la quantité ajoutée et le nom du produit
 
+  //alert("Vous avez ajouté " + produit + " à votre panier. Quantité: " + (qte + 1));
+  // Mettre à jour le nombre total d'articles dans le panier
   
 }
-
-
-      
-    
+ function func(info) 
+            {
+               
+               alert(info + "");
+               
+            }
